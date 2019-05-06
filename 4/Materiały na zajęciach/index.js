c9811ae95661dff8e159bef49b125156521ca5bc
Grzegorz Tomaszewski <aroxd3@gmail.com>
2019-05-06 17:06:48 +0200

    added new files

---
+++ 4/Materiały na zajęciach/index.js
@@ -0,0 +1,34 @@
+//deklaracja tablicy
+
+//length
+
+//dodawanie elementów - push lub po indeksie, unshift
+
+//usuwanie elementów - pop, shift
+
+//join
+
+//sort
+
+//łączenie tablic - concat lub spread operator
+
+
+//wycinanie kawałka tablicy - slice
+
+
+//usuwanie i dodawanie elementów - splice
+
+//map
+
+//filtrowanie
+
+//indexOf
+
+//pętla dla tablic -for, forEach
+
+/*Zad.1
+Napisz funkcję, która zwraca sumę dwóch najmniejszych liczb z tablicy.
+
+Zad.2
+Napisz funkcję, która usuwa najmniejszą liczbę z tablicy i zwraca tę tablicę (nie zmieniając kolejności elementów w tablicy).
+*/
